57020b5b10c00d09add7ab56c5d70ae517ab7459
Christian Raue <raue.christian@live.de>
2023-01-14 14:28:31 +0100

    changed exit code again

--- bin/_rl/rl_manager.py
+++ bin/_rl/rl_manager.py
@@ -52,7 +52,7 @@ class RlManager:
             Output.critical("System is not running!")
         Output.info("Stopping system...")
         command = Command(f"pkill -15 -f {Paths.RL_RUN}")
-        if command.get_returncode() != 0:
+        if command.get_returncode() != -15:
             Output.error("Error stopping device!")
 
     @classmethod
