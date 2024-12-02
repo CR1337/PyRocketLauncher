775d2ee85f1e12ae7495e748afc005746112d6bd
Christian Raue <captain_2@gmx.de>
2024-12-02 21:06:14 +0100

    correct path

--- backend/system.py
+++ backend/system.py
@@ -62,7 +62,7 @@ class System:
         if not Instance.on_pi():
             return False
         output = subprocess.check_output(
-            ["rl", "status", "get", "update"]
+            ["bin/rl", "status", "get", "update"]
         ).decode(encoding='ascii')
         status = output.split()[-1]
         if status == 'ahead':
